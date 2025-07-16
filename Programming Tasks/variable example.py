d6e24f88f5d2578d4a8200fbdb84c6cf2d4da371
arshan404 <fcshayan20@gmail.com>
2025-07-16 18:43:10 +0330

    variable example

---
+++ Programming Tasks/variable example.py
@@ -0,0 +1,10 @@
+pizza_price = 14
+pizza_num = 8
+dogh_price = 400
+doghe_num = 6
+jaa_price = 6000
+jaa_num = 3 
+
+kol_kharje = (pizza_price * pizza_num)+(dogh_price*doghe_num)+(jaa_price * jaa_num)
+
+print(kol_kharje)
