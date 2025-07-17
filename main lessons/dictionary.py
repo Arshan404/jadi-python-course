4dd308b61c0deaaa3a15ac8f33398c913a7a6ff1
arshan404 <fcshayan20@gmail.com>
2025-07-17 17:30:52 +0330

    dictioanary

---
+++ main lessons/dictionary.py
@@ -0,0 +1,14 @@
+nomre = {
+    "jadi":{
+        "riazi" : 12 , 
+        "programming" : 20 , 
+        "motoon": 2
+    } , 
+    "sara" : {
+        "riazi" : 18 ,
+        "programming" : 20 ,
+        "motoon" : 20 
+    }
+}
+
+print(nomre["jadi"]["riazi"])
