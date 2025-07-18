508a455defcae1b3a6045e4bbb40054452b82635
arshan404 <fcshayan20@gmail.com>
2025-07-18 13:08:09 +0330

    if sample 2

---
+++ Programming Tasks/if sample 2.py
@@ -0,0 +1,15 @@
+age=20
+
+if age<20:
+    vaziat="young"
+    print("you are young keep going")
+if age>20:
+    vaziat="old"
+    print("you are getting old man")
+    child_age=8
+    print("you're child age is",child_age,)
+if age ==20:
+    vaziat="teen"
+    print("you are fit haha!!")
+    
+
