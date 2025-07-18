4143519c3d5459157a703b1accdefe546606956e
arshan404 <fcshayan20@gmail.com>
2025-07-18 13:08:20 +0330

    function sample

---
+++ Programming Tasks/function sample.py
@@ -0,0 +1,13 @@
+for name in('shayan','dina','mohsen'):
+    print('hi',name,)
+
+def hello(names):
+    print(' hi '+names)
+for name in ['shayan','dina','mohsen']:
+    hello(name)
+
+def jam(x,y):
+    print(x+y)
+jam(4,5)
+jam(5,19)
+jam('jadi','mostafa')
