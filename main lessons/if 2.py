1a749168fec1efa29563d714103cb5c154f673a3
arshan404 <fcshayan20@gmail.com>
2025-07-24 21:59:13 +0330

    if sample 2

---
+++ main lessons/if 2.py
@@ -0,0 +1,17 @@
+action = 'shoot'
+score = 0
+
+if action =='shoot':
+    print("oh shoot kard!")
+elif action =='pass':
+    print('pass dad be doostesh')
+elif action == "goal" :
+    print("goaaaaaal")
+    score +=1
+else:
+    print("che kardi?")
+
+if score> 0 :
+    print(f"oh barande shodi{score}")
+else:
+    print("bazi cherti bood")
