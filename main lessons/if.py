dfa02e0e1b2940a973913d5d49e55c4ccf5a9dca
arshan404 <fcshayan20@gmail.com>
2025-07-24 21:52:02 +0330

    if sample

---
+++ main lessons/if.py
@@ -0,0 +1,8 @@
+sen = 18
+
+if sen < 18:
+    print("salam bache")
+elif sen < 40:                    #در پایتون دستوری به اسم else if نداریم
+    print("salam mian sal")
+else:
+    print("salam mosen")
