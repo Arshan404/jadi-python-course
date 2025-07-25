d774e81b89796caa2652f6ab0bde73be5f201f44
arshan404 <fcshayan20@gmail.com>
2025-07-25 16:19:51 +0330

    while loop

---
+++ main lessons/while.py
@@ -0,0 +1,27 @@
+a = 0
+
+print("I'm starting ")
+
+while a <= 5:
+    print(f"a is {a}")
+    a += 1
+print("I'm done")
+print(f"finishing with {a}")
+
+
+b = 0
+
+while b <100 :
+    print(b)     
+    b += 1    # اگه این خط رو نمینوشتیم تا بی نهاین 0 رو چاپ میکرد 
+              # اگه میخوایین برنامه متوقف شه کافیه ctrl +c بگیری
+
+print(" oh! the fuel is low ")
+
+f = 12
+while f <= 100 :
+    #refueling the car
+    #do whatever needed
+    f += 1
+print(f" fuel is {f}")
+print("barghashdim be jade")
