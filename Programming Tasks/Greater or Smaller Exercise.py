25a48ce3ce3feae0c5c55036070c102b4a87c3e3
arshan404 <fcshayan20@gmail.com>
2025-08-02 09:32:41 +0330

    grater or smaller

---
+++ Programming Tasks/Greater or Smaller Exercise.py
@@ -0,0 +1,10 @@
+def is_greater( a , b):
+    if a > b :
+        return True
+    else:
+        return False
+    
+a = int(input("please enter your first number : "))
+b = int(input("please enter your second number : "))
+
+print(is_greater(a,b))
