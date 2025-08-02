26b4a85744f56d2332b62a6713f7bcfbcdf366d6
arshan404 <fcshayan20@gmail.com>
2025-08-02 23:36:19 +0330

    scope explain

---
+++ main lessons/scope.py
@@ -0,0 +1,9 @@
+def hello():
+    #global name
+    name = 'hasan'
+    print(f'in function name is {name}')
+
+name = 'jadi'
+print(f'first , name is {name}')
+hello()
+print(f"second , name is {name}")
