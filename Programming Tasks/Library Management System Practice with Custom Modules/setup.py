a3233cb95d6e43300be1a5bb8e813f22fc9c8b6a
arshan404 <fcshayan20@gmail.com>
2025-08-07 23:24:34 +0330

    fixed setup file

--- Programming Tasks/Library Management System Practice with Custom Modules/setup.py
+++ Programming Tasks/Library Management System Practice with Custom Modules/setup.py
@@ -7,8 +7,8 @@ setup(
     version = '0.1',
     packages = find_packages(),
     description = 'A simple library management system',
-    author =  'Shayan'
-    author_email = 'fcshayan20@gmail.com'
+    author = 'Shayan',
+    author_email = 'fcshayan20@gmail.com',
     classifiers = [
     'programming language ::python::3',
     'operating system :: Os Independent',
