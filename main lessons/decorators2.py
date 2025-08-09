5630488a88779441e49ade1b2c791e006ee1a0c6
arshan404 <fcshayan20@gmail.com>
2025-08-09 14:37:21 +0330

    say hello v2

--- main lessons/decorators2.py
+++ main lessons/decorators2.py
@@ -1,10 +1,21 @@
-def say_hello():
+import time
+
+def is_it_zoj():
     import datetime
     now = datetime.datetime.now()
     minute = now.minute
-    if minute % 2 == 0 :
+    return minute % 2 == 0
+
+
+def say_hello():
+    if is_it_zoj():
         print("Hissss")
     else:
         print("salam! man Injam ")
-        
+
+def say_bye():
+    if is_it_zoj():
+        print("bye bye!")
+
 say_hello()
+say_bye()
