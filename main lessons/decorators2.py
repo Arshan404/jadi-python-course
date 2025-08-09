3e60b7afb945983a252640c9212271d6bee4c691
arshan404 <fcshayan20@gmail.com>
2025-08-09 14:58:23 +0330

    say hello v4

--- main lessons/decorators2.py
+++ main lessons/decorators2.py
@@ -1,18 +1,22 @@
 def run_on_zoj(f):
-    import datetime
-    now = datetime.datetime.now()
-    minute = now.minute
-    if minute % 2 == 0 :
-        f()
-    else:
-        print('hissss')
+    def wrapper():
+        import datetime
+        now = datetime.datetime.now()
+        minute = now.minute
+        if minute % 2 == 0:
+            f()
+        else:
+            print('hissss')
+    return wrapper
 
-
+@run_on_zoj
 def say_hello():
-        print("salam! man Injam ")
+    print("salam! man Injam ")
 
+@run_on_zoj
 def say_bye():
-        print("bye bye!")
+    print("bye bye!")
 
-run_on_zoj(say_hello)
-run_on_zoj(say_bye)
+say_hello()
+say_bye()
+
