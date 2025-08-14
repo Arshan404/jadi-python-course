21a555e424d968d7d9c027a1cb0d79424ab24684
arshan404 <fcshayan20@gmail.com>
2025-08-14 21:45:27 +0330

    editing image part two

--- main lessons/mypillow.py
+++ main lessons/mypillow.py
@@ -16,6 +16,23 @@ unfixed  = img.rotate(90)
 #unfixed.show()
 u = img.crop((0, 200, 630, 430))  # left=0, top=200, right=630, bottom=430
 # نمایش تصویر کراپ شده
-u.show()
-img.paste(u , (0,0))
-img.show()
+#u.show()
+#img.paste(u , (0,0))
+#img.show()
+print(img.getpixel((100,100)))    #based on RGB
+#img.putpixel((100,100) , (255,0,0))
+#img.show()
+'''
+for i in range(10):
+    for j in range(10):
+        img.putpixel((100+i,100+j) , (0,0,250))
+'''        
+#img.show()
+for i in range(10):
+    for j in range(10):
+        img.putpixel((100+i, 100+j), (i*10, j*10, 0))  # اضافه کردن کانال B
+
+#img.show()
+#img.putalpha(30)
+#img.show()
+
