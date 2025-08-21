6e05791e0c87c111048e6609ffc396125883c7ea
arshan404 <fcshayan20@gmail.com>
2025-08-21 07:27:09 +0330

    advanced numbers

---
+++ main lessons/Advanced concept of numbers.py
@@ -0,0 +1,15 @@
+print(12)
+print(hex(12))   #برمبنای 16
+print(oct(12))   #برمبنای 8
+print(bin(12))
+
+for i in range(0x10):
+    print(i)
+
+print(int('45' , 8))    #عدد 45 برمبنای 8
+print(2**10)
+print(pow(2,10))        #به توان رسوندن
+print(pow(2,10,100))    #2 به توان 100 ولی باقی ماندش به 100 رو نشون بده
+print(abs(4))            #قدر مطلق
+print(abs(-4))
+print(round(3.14))
